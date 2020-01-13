af8ea5ee9b4c70732c75e04e7b19cbe7936be61d
pan-727 <59717744+pan-727@users.noreply.github.com>
2020-01-13 18:29:26 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
-window.onload=function(){
-	
-	var page1=document.getElementById("page1");
-	var page2=document.getElementById("page2");
-	var page3=document.getElementById("page3");
-	
-	var music=document.getElenebtById(music);
-	var audio=document.getElementsByTagName("audio")[0];
-	
-	audio.addEventListener("ended",function(event){
-		music.setAttribute("class","");
-	},false);
-	
-	music.addEventListener("touchstart",function(event){
+window.onload = () => {
+	const page1 = document.getElementById('page1');
+	const page2 = document.getElementById('page2');
+	const page3 = document.getElementById('page3');
+	const music = document.getElementById('music');
+	const audio = document.getElementsByTagName('audio')[0];
+
+	audio.addEventListener('ended', () => {
+		music.style.animationPlayState="paused";
+		music.style.webkitAnimationPlayState="paused";
+	});
+
+	music.addEventListener('touchstart', () => {
 		if(audio.paused){
 			audio.play();
-			this.setAttribute("class","play");
-			
+			music.style.animationPlayState="running";
+			music.style.webkitAnimationPlayState="running";
 		}else{
 			audio.pause();
-			this.setAttribute("class","");
-		};
+			music.style.animationPlayState="paused";
+			music.style.webkitAnimationPlayState="paused";
+		}
 	},false);
-	
-	page1.addEventListener("touchstart",function(event){
-		page1.style.display="none";
-		page2.style.display="block";
-		page3.style.display="block";
-		page3.style.top="100%";
-		
-		setTimeout(function(){
-			page2.setAttribute("class","page fadeOut");
-			page3.setAttribute("class","page fadeIn");
-		},5500);
+
+
+	page1.addEventListener('touchstart', () => {
+		page1.style.display = 'none';
+		page2.style.display = 'block';
+		page3.style.display = 'block';
+		page3.style.top = '100%'; 
+
+		setTimeout(() => {
+			page2.setAttribute('class','page fadeOut');
+			page3.setAttribute('class','page fadeIn');
+		},5500)
 	},false);
-};
+}
